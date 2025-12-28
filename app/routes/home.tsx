25900925d58463e82fc4db189f4ce46f7a544c2b
Vansh Chitransh <vanshchitransh32@gmail.com>
2025-12-28 13:35:34 +0530

    projectSetup - landingPage - initialTypes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants/index";
+import Resume from "~/component/Resume";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Analyse your resume for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    {/* {window.puter.auth.} */}
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submissions with AI-powered feedback.</h2>
+      </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <Resume key={resume.id} resume={resume}></Resume>
+          ))}
+        </div>
+      )}
+    </section>
+    
+  </main>
 }
--- learning.js
+++ learning.js
@@ -1,4 +1,7 @@
 /**
  * Use pnpm create vite@latest and then a dot at the end to create the project in the current folder itself, that dot will not create one more folder. 
  * 
+ * The order of plugins in vite.config.ts files defines the execution of the those plugins. 
+ * 
+ * Use proper html tags, section tag, nav tag, 
  */
