6ed37cc0dbdc2f688d8965dff8bf8fee6a64cfad
Vansh Chitransh <vanshchitransh32@gmail.com>
2025-12-29 13:31:36 +0530

    Added auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "constants/index";
 import Resume from "~/component/Resume";
+import { usePuterStore } from "~/lib/putter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if(!auth.isAuthenticated){
+      navigate('/auth?next=/')
+    }
+  }, [auth.isAuthenticated])
+
+  
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
     {/* {window.puter.auth.} */}
--- learning.js
+++ learning.js
@@ -3,5 +3,12 @@
  * 
  * The order of plugins in vite.config.ts files defines the execution of the those plugins. 
  * 
- * Use proper html tags, section tag, nav tag, 
+ * Use proper html tags, section tag, nav tag, main tag (learn html tags and why which tag is used?)
+ * 
+ * We have to give meta data according to each page
+ * 
+ * 
+ * Use tricks like, if someone tried to access a protected route without login, redirect them to login page and after login redirect them to the protected route they wanted to access.
+ * 
+ * 
  */
